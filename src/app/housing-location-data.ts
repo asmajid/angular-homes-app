37a2c1f2ec3ab8db5cd541b7b89eb1552051a5c6
Asmaa Majid <majid.asmaa2001@gmail.com>
2024-04-20 19:41:33 +0100

    Added HousingLocationData array containing information about different housing locations

---
+++ src/app/housing-location-data.ts
@@ -0,0 +1,94 @@
+import { HousingLocation } from "./housing-location";
+
+export const HousingLocationData: HousingLocation[] = [
+  {
+    id: 1,
+    name: 'Marhaba Residences',
+    city: 'Casablanca',
+    state: 'Casablanca-Settat',
+    photo: '../assets/residence-1.jpg',
+    availableUnits: 3,
+    wifi: true,
+    laundry: true
+  },
+  {
+    id: 2,
+    name: 'Majestic Mansions',
+    city: 'Rabat',
+    state: 'Rabat-Salé-Kénitra',
+    photo: '../assets/residence-1.jpg',
+    availableUnits: 2,
+    wifi: true,
+    laundry: false
+  },
+  {
+    id: 3,
+    name: 'Atlas Apartments',
+    city: 'Marrakech',
+    state: 'Marrakech-Safi',
+    photo: '../assets/residence-2.jpg',
+    availableUnits: 5,
+    wifi: true,
+    laundry: true
+  },
+  {
+    id: 4,
+    name: 'Sahara Suites',
+    city: 'Agadir',
+    state: 'Souss-Massa',
+    photo: '../assets/residence-3.jpg',
+    availableUnits: 1,
+    wifi: false,
+    laundry: true
+  },
+  {
+    id: 5,
+    name: 'Royal Residency',
+    city: 'Fes',
+    state: 'Fes-Meknès',
+    photo: '../assets/residence-4.webp',
+    availableUnits: 4,
+    wifi: true,
+    laundry: true
+  },
+  {
+    id: 6,
+    name: 'Golden Gate Apartments',
+    city: 'Tangier',
+    state: 'Tanger-Tetouan-Al Hoceima',
+    photo: '../assets/residence-5.jpg',
+    availableUnits: 0,
+    wifi: true,
+    laundry: false
+  },
+  {
+    id: 7,
+    name: 'Oasis Towers',
+    city: 'Meknes',
+    state: 'Fes-Meknes',
+    photo: '../assets/residence-6.webp',
+    availableUnits: 2,
+    wifi: true,
+    laundry: true
+  },
+  {
+    id: 8,
+    name: 'Dreamland Residences',
+    city: 'Essaouira',
+    state: 'Marrakech-Safi',
+    photo: '../assets/residence-7.jpg',
+    availableUnits: 3,
+    wifi: true,
+    laundry: false
+  },
+  {
+    id: 9,
+    name: 'Palm Palace',
+    city: 'Ouarzazate',
+    state: 'Drâa-Tafilalet',
+    photo: '../assets/residence-8.jpg',
+    availableUnits: 1,
+    wifi: true,
+    laundry: true
+  }
+]
