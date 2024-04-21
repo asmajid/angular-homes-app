d26c4681173cc19b81f20b5e5443b22e1f60a714
Asmaa Majid <majid.asmaa2001@gmail.com>
2024-04-21 13:46:44 +0100

    Added Housing service

---
+++ src/app/housing.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housing-location';
+import { HousingLocationData } from './housing-location-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  housingLocations: HousingLocation[] = HousingLocationData
+
+  constructor() { }
+
+  getAllHousinLocation():HousingLocation[]{
+   return this.housingLocations
+  }
+
+  getHousingLocationById(id:number):HousingLocation | undefined{
+    return this.housingLocations.find(housingLocation =>{
+      return housingLocation.id === id
+    })
+  }
+}
